84fce739491961ce5e6571068ec23f84508d0eeb
manataliekeithse <141150584+manataliekeithse@users.noreply.github.com>
2024-10-08 20:46:27 +0800

    final

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+
+import { router as contactsRouter } from "./routes/api/contactsRouter.js";
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/contacts", contactsRouter);
+
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: err.message });
+});
+
+export { app };
